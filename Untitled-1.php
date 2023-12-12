28cce5ff14f81fbd85f5acdf0a603b6b70a69d1e
Nikhil Sakalabhaktula <es836@smartnet.cxm>
2023-12-12 10:58:13 +0530

    tes

---
+++ Untitled-1.php
@@ -0,0 +1,85 @@
+<p><b>16.1 Assignment and other dealings.</b>
+</p>
+<p> (a) BODYTEQ may at any time assign, transfer, mortgage, charge, subcontract or deal in any other manner with all or
+    any of its rights under the Contract and may subcontract or delegate in any manner any or all of its obligations
+    under the Contract to any third party.
+</p>
+<p>(b) The Customer shall not, without the prior written consent of BODYTEQ, assign, transfer, charge, subcontract,
+    declare a trust over or deal in any other manner with all or any of its rights or obligations under the Contract.
+</p>
+<p><b>16.2 Notices.
+    </b> </p>
+<p> (a) Any notice or other communication given to a party under or in connection with this Contract shall be in
+    writing, addressed to that party at its registered office (if it is a company) or its principal place of business
+    (in any other case) or such other address as that party may have specified to the other party in writing in
+    accordance with this clause, and shall be delivered personally or sent by prepaid first-class post or other next
+    working day delivery service, or by commercial courier, fax [or e-mail].
+</p>
+<p>(b) A notice or other communication shall be deemed to have been received: if delivered personally, when left at the
+    address referred to in clause 12.2(a); if sent by pre-paid first class post or other next working day delivery
+    service, at 9.00 am on the second Business Day after posting; if delivered by commercial courier, on the date and at
+    the time that the courier's delivery receipt is signed; or, if sent by fax or e-mail, one Business Day after
+    transmission.
+</p>
+<p>(c) The provisions of this clause shall not apply to the service of any proceedings or other documents in any legal
+    action.
+</p>
+<p><b>
+        16.3 Severance.
+
+    </b> </p>
+<p> (a) If any provision or part-provision of the Contract is or becomes invalid, illegal or unenforceable, it shall be
+    deemed modified to the minimum extent necessary to make it valid, legal and enforceable. If such modification is not
+    possible, the relevant provision or part-provision shall be deemed deleted. Any modification to or deletion of a
+    provision or part-provision under this clause shall not affect the validity and enforceability of the rest of the
+    Contract.
+</p>
+<p> (b) If [one party gives notice to the other of the possibility that] any provision or part-provision of this
+    Contract is invalid, illegal or unenforceable, the parties shall negotiate in good faith to amend such provision so
+    that, as amended, it is legal, valid and enforceable, and, to the greatest extent possible, achieves the intended
+    commercial result of the original provision.
+</p>
+<p><b>16.4</b> Waiver. A waiver of any right under the Contract or law is only effective if it is in writing and shall
+    not be
+    deemed to be a waiver of any subsequent breach or default. No failure or delay by a party in exercising any right or
+    remedy under the Contract or by law shall constitute a waiver of that or any other right or remedy, nor prevent or
+    restrict its further exercise of that or any other right or remedy. No single or partial exercise of such right or
+    remedy shall prevent or restrict the further exercise of that or any other right or remedy.
+</p>
+<p><b>16.5</b> No partnership or agency. Nothing in the Contract is intended to, or shall be deemed to, establish any
+    partnership or joint venture between any of the parties, nor constitute either party the agent of another party for
+    any purpose. Neither party shall have authority to act as agent for, or to bind, the other party in any way.
+</p>
+<p><b>16.6</b> Third parties. A person who is not a party to the Contract shall not have any rights to enforce its
+    terms.
+</p>
+<p><b> 16.7</b> Variation. Except as set out in these Conditions, no variation of the Contract, including the
+    introduction of
+    any additional terms and conditions shall be effective unless it is agreed in writing and signed by BODYTEQ.
+</p>
+
+<p><b>16.8</b> Governing law. This agreement and any dispute or claim arising out of or in connection with it or its
+    subject
+    matter or formation (including non-contractual disputes or claims) shall be governed by and construed in accordance
+    with the law of England and Wales.
+</p>
+<p><b>16.9</b> Jurisdiction Each party irrevocably agrees that the courts of England and Wales shall have exclusive
+    jurisdiction to settle any dispute or claim arising out of or in connection with this Contract or its subject matter
+    or formation (including non-contractual disputes or claims).
+</p>
+<p> </p>
+<p> </p>
+<p> </p>
+<p> </p>
+<p> </p>
+<p> </p>
+<p> </p>
+<p> </p>
+<p> </p>
+<p> </p>
+<p> </p>
+<p> </p>
+<p> </p>
+<p> </p>
+<p> </p>
+<p> </p>
